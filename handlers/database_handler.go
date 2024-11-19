82af18d3964428e4c87980e7570a366d673e4e9b
sami <msami.bese22seecs@student.nust.edu.pk>
2024-11-19 22:45:14 +0500

    .

---
+++ handlers/database_handler.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func ListDatabasesHandler(c *gin.Context, driver neo4j.Driver) {
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
+	defer session.Close()
+
+	result, err := session.Run("SHOW DATABASES", nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var databases []string
+	for result.Next() {
+		dbName, _ := result.Record().Get("name")
+		databases = append(databases, dbName.(string))
+	}
+
+	c.JSON(http.StatusOK, gin.H{"databases": databases})
+}
+
+// Count nodes in a database
+func CountNodesHandler(c *gin.Context, driver neo4j.Driver) {
+	dbName := c.Param("dbname")
+
+	session := driver.NewSession(neo4j.SessionConfig{DatabaseName: dbName, AccessMode: neo4j.AccessModeRead})
+	defer session.Close()
+
+	result, err := session.Run("MATCH (n) RETURN COUNT(n) AS count", nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if result.Next() {
+		nodeCount := result.Record().Values[0].(int64)
+		c.JSON(http.StatusOK, gin.H{"node_count": nodeCount})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"node_count": 0})
+	}
+}
--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"log"
 	"my-go-project/routers"
+	"my-go-project/utils"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func main() {
-	driver, err := initializeDriver("bolt://localhost:7687", neo4j.NoAuth())
+	driver, err := utils.InitializeDriver("bolt://localhost:7687", neo4j.NoAuth())
 	if err != nil {
 		log.Fatalf("Failed to initialize Neo4j driver: %v", err)
 	}
 	defer driver.Close()
 
 	router := routers.SetupRouter(driver)
-	log.Fatal(router.Run(":8080")) // Start the server on port 8080
+	log.Fatal(router.Run(":8070"))
 }
